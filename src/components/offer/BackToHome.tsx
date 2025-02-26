4159ae8cc6b2887c9961629d9276ef9554a5ff58
kishi_masahiro <ksmshr1988@gmail.com>
2025-02-26 22:17:21 +0900

    add offer_check_page

---
+++ src/components/offer/BackToHome.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const BackToHome: React.FC = () => {
+  return (
+    <a
+      href="/home"
+      className="self-center mt-10 text-base font-semibold text-black underline"
+    >
+      ホームにもどる
+    </a>
+  );
+};
